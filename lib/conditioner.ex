566ebc49fae3f2cf457f256add38814ca7e6b7d7
Jakub Jasiulewicz <jakub.jasiulewicz@gmail.com>
2023-04-12 20:35:04 +0200

    Update mix.exs

--- lib/conditioner.ex
+++ lib/conditioner.ex
@@ -1,12 +1,63 @@
 defmodule Conditioner do
+  @moduledoc """
+
+  Conditioner allows you to define and process conditional logic in separated way:
+
+  1. Create logical representation of conditions:
+
+  ```elixir
+  conditions = %{
+      "and" => [
+        ["filename", "containsfn", "he"],
+        ["filename", "containsfn", "lo"],
+        ["otherrule", "contains", "lo"],
+        %{
+          "or" => [
+            ["filename", "containsfn", "bo"],
+            ["filename", "containsfn", "he"],
+            %{"and" => true}
+          ]
+        }
+      ]
+    }
+
+  ```
+
+  2. Define matcher module with rules:
+
+  ```elixir
+  defmodule SomeMatcher do
+    use Conditioner.Matcher
+
+    def match(["filename", "containsfn", str], _original_value) do
+      fn val ->
+        String.contains?(val, str)
+      end
+    end
+
+    def match(["otherrule", "contains", str], value) do
+      String.contains?(value, str)
+    end
+  end
+  ```
+
+  3. Verify conditions by calling matcher with rules:
+
+  ```elixir
+  result = Conditioner.match?(conditions, "hello", SomeMatcher)
+  ```
+
+  """
   def match?([], value, matcher) do
     apply(matcher, :match_empty_conditions, [value])
   end
 
+  @spec match?(map(), any(), any()) :: boolean()
   def match?(conditions, value, matcher) when is_map(conditions) do
     match?([conditions], value, matcher)
   end
 
+  @spec match?(list(), any(), any()) :: boolean()
   def match?(conditions, value, matcher) do
     conditions
     |> Enum.map(&parse_condition(&1, value, matcher))
--- mix.exs
+++ mix.exs
@@ -1,28 +1,47 @@
 defmodule Conditioner.MixProject do
   use Mix.Project
-
+  @version "0.1.0"
+  @url_docs "http://hexdocs.pm/conditioner"
+  @url_github "https://github.com/up2jj/conditioner"
   def project do
     [
       app: :conditioner,
-      version: "0.1.0",
+      name: "Conditioner",
+      description: "Conditional logic utility",
+      package: %{
+        files: [
+          "lib",
+          "mix.exs",
+          "LICENSE"
+        ],
+        licenses: ["Apache-2.0"],
+        links: %{
+          "Docs" => @url_docs,
+          "GitHub" => @url_github
+        },
+        source_url: @url_github,
+        maintainers: ["Jakub Jasiulewicz"]
+      },
+      docs: [
+        source_ref: "#{@version}",
+        source_url: @url_github
+      ],
+      version: @version,
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.29", only: :dev, runtime: false}
     ]
   end
 end
